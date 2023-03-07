55fe6a9ee0157525b75d24eb10c5427d88594d49
Glenn Rempe <glenn@truestamp.com>
2023-03-07 11:26:34 -0500

    chore: convert functions to function declaration style and order by function name

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ import { promisify } from "node:util"
 import bs58 from "bs58"
 import padStart from "lodash/padStart"
 
-export const hashLongToken = (longToken: string) =>
-  createHash("sha256").update(longToken).digest("hex")
-
 export interface GenerateAPIKeyOptions {
   keyPrefix?: string
   shortTokenPrefix?: string
@@ -13,12 +10,31 @@ export interface GenerateAPIKeyOptions {
   longTokenLength?: number
 }
 
-export const generateAPIKey = async ({
+export function checkAPIKey(
+  token: string,
+  expectedLongTokenHash: string
+): boolean {
+  return hashLongToken(extractLongToken(token)) === expectedLongTokenHash
+}
+
+export function extractLongToken(token: string) {
+  return token.split("_").slice(-1)?.[0]
+}
+
+export function extractLongTokenHash(token: string) {
+  return hashLongToken(extractLongToken(token))
+}
+
+export function extractShortToken(token: string) {
+  return token.split("_")?.[1]
+}
+
+export async function generateAPIKey({
   keyPrefix,
   shortTokenPrefix = "",
   shortTokenLength = 8,
   longTokenLength = 24,
-}: GenerateAPIKeyOptions = {}) => {
+}: GenerateAPIKeyOptions = {}) {
   if (!keyPrefix) return {}
 
   const generatedRandomBytes = promisify(randomBytes)
@@ -49,22 +65,16 @@ export const generateAPIKey = async ({
   return { shortToken, longToken, longTokenHash, token }
 }
 
-export const extractLongToken = (token: string) =>
-  token.split("_").slice(-1)?.[0]
+export function getTokenComponents(token: string) {
+  const longToken = extractLongToken(token)
+  return {
+    longToken,
+    shortToken: extractShortToken(token),
+    longTokenHash: hashLongToken(longToken),
+    token,
+  }
+}
 
-export const extractShortToken = (token: string) => token.split("_")?.[1]
-
-export const extractLongTokenHash = (token: string) =>
-  hashLongToken(extractLongToken(token))
-
-export const getTokenComponents = (token: string) => ({
-  longToken: extractLongToken(token),
-  shortToken: extractShortToken(token),
-  longTokenHash: hashLongToken(extractLongToken(token)),
-  token,
-})
-
-export const checkAPIKey = async (
-  token: string,
-  expectedLongTokenHash: string
-) => hashLongToken(extractLongToken(token)) === expectedLongTokenHash
+export function hashLongToken(longToken: string) {
+  return createHash("sha256").update(longToken).digest("hex")
+}
