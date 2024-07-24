c87b23e00222b37d733a57bbdc399cdfeb3479cc
sam bacha <sam@manifoldfinance.com>
2024-07-23 20:50:55 -0700

    fix(eslint): fix typescript support and configuration format

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+);
